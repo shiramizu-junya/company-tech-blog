3e5ff6f2bec206dc8953622b63ef4aa535b47bbc
Shiramizu_Junya <jyunya.pg.7231@gmail.com>
2024-07-20 15:13:57 +0900

    feat: add environment building

---
+++ 2024-07-22-chrome-webrequest-api-sample/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+	entry: {
+		content_script: './src/index.ts',
+		background: './src/background/background.ts',
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js',
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, 'dist'),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	plugins: [
+		new webpack.ProgressPlugin(),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, './manifest'),
+					to: path.resolve(__dirname, './dist')
+				},
+				{
+					from: path.resolve(__dirname, './public'),
+					to: path.resolve(__dirname, './dist')
+				},
+			]
+		})
+	],
+};
